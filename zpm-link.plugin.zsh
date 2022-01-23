4aaaba94eabc7b3bce2a9883e2a6e598bea97127
Grigorii Horos <horosgrisa@gmail.com>
2022-01-23 02:11:57 +0200

    CHange plugin

--- zpm-link.plugin.zsh
+++ zpm-link.plugin.zsh
@@ -5,5 +5,5 @@ autoload -Uz zpm-link
 _zpm_extend_commands+=('link:Link plugin into zpm plugins dir')
 
 _zpm_link_completion() {
-    _arguments '*:directories:_directories'
+    _arguments '*:directories:_files'
 }
